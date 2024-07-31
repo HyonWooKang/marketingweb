833904063af00ec440b52c6bd94ece3699d04aea
HyonWooKang <87benzenes@gmail.com>
2024-07-31 16:11:43 +0900

    scss 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import { RecoilRoot } from "recoil";
