3bc9837f81c5788e2f021a6ee08a4e99a9deae53
HyonWooKang <87benzenes@gmail.com>
2024-08-01 17:03:46 +0900

    Home 화면에 내비바 제작 및 컨테이너 분리

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { Helmet } from "react-helmet-async";
+// import "";
 
 const Home = () => {
   return (
@@ -7,16 +8,37 @@ const Home = () => {
         <title>Main Page</title>
         {/* <link rel="canonical" href="https://www.test.com/" /> */}
       </Helmet>
-      <div className="head">
-        <nav role="navigation" id="gnb" aria-label="메인 메뉴">
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+      <div className="main-container">
+        <nav
+          className="navbar"
+          role="navigation"
+          id="gnb"
+          aria-label="메인 메뉴"
+        >
+          <div className="navbar-logo">
+            <i className="fa-soild"></i>
+            {/* <img /> */}
+            <a>HYONWOO CAR</a>
+          </div>
+          <ul className="navbar__menu">
+            <li>
+              <a>메뉴1</a>
+            </li>
+            <li>
+              <a>메뉴2</a>
+            </li>
+            <li>
+              <a>메뉴3</a>
+            </li>
+            <li>
+              <a>메뉴4</a>
+            </li>
           </ul>
         </nav>
       </div>
+      <main className="container">
+        <div></div>
+      </main>
     </>
   );
 };
